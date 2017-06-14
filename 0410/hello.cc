3154be59bc0962cbf10882a4a91147e6911b937d
luan2501 <1020105056@qq.com>
2017-06-14 12:11:04 +0800

    test file

---
+++ 0410/hello.cc
@@ -0,0 +1,21 @@
+ ///
+ /// @file    hello.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2017-06-14 12:04:19
+ ///
+#include <stdio.h>
+
+#include "stdlib.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char **argv)
+{
+	printf("hello,world\n");
+	cout<<"hello,world"<<endl;
+	return 0;
+
+}
+
